1fd6cfba233c4de0f452c861b897c3bae97c1c4a
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-09-27 11:20:52 +0200

    first networks

--- env-main.tf
+++ env-main.tf
@@ -6,27 +6,27 @@ resource "google_compute_network" "vpc-intellibot" {
 }
 
 # Subnet
-resource "google_compute_subnetwork" "nets-intellibot-A" {
+resource "google_compute_subnetwork" "nets-intellibot-a" {
   count     = length(var.network-definition)
   region    = var.gcp-region
   private_ip_google_access = true
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.network-definition,count.index)[1]
-  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-A"  
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-a"  
 }
-resource "google_compute_subnetwork" "nets-intellibot-B" {
+resource "google_compute_subnetwork" "nets-intellibot-b" {
   count     = length(var.network-definition)
   region    = var.gcp-region
   private_ip_google_access = true
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.network-definition,count.index)[2]
-  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-B"  
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-b"  
 }
-resource "google_compute_subnetwork" "nets-intellibot-C" {
+resource "google_compute_subnetwork" "nets-intellibot-c" {
   count     = length(var.network-definition)
   region    = var.gcp-region
   private_ip_google_access = true
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.network-definition,count.index)[3]
-  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-C"  
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-c"  
 }
