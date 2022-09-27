e40f7dbddfd50077234d4bb11f5885f66c4cd0d2
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-09-27 11:44:06 +0200

    adding preprod

--- env-main.tf
+++ env-main.tf
@@ -1,4 +1,3 @@
-
 resource "google_compute_network" "vpc-intellibot" {
   count     = length(var.network-definition)  
   name      = "vpc-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}"
@@ -29,4 +28,29 @@ resource "google_compute_subnetwork" "nets-intellibot-c" {
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.network-definition,count.index)[3]
   name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-c"  
+}
+
+resource "google_compute_network_peering" "peering-dev-to-preprod" {
+  count = length(var.vpc-spoke)
+
+  name         = "peering-${google_compute_network.vpc-intellibot[0].name}-to-${google_compute_network.vpc-intellibot[1].name}"
+  network      = google_compute_network.vpc-intellibot[0].id
+  peer_network = google_compute_network.vpc-intellibot[1].id
+  export_custom_routes = true
+}
+resource "google_compute_network_peering" "peering-dev-to-prod" {
+  count = length(var.vpc-spoke)
+
+  name         = "peering-${google_compute_network.vpc-intellibot[0].name}-to-${google_compute_network.vpc-intellibot[2].name}"
+  network      = google_compute_network.vpc-intellibot[0].id
+  peer_network = google_compute_network.vpc-intellibot[2].id
+  export_custom_routes = true
+}
+resource "google_compute_network_peering" "peering-preprod-to-prod" {
+  count = length(var.vpc-spoke)
+
+  name         = "peering-${google_compute_network.vpc-intellibot[1].name}-to-${google_compute_network.vpc-intellibot[2].name}"
+  network      = google_compute_network.vpc-intellibot[1].id
+  peer_network = google_compute_network.vpc-intellibot[2].id
+  export_custom_routes = true
 }
