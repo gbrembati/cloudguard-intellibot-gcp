bad8fd82ba4d461bff51d5748da1370438e5254e
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-09-27 11:04:05 +0200

    first networks

--- env-main.tf
+++ env-main.tf
@@ -1,25 +1,25 @@
 
 
 resource "google_compute_network" "vpc-intellibot" {
-  count     = lenth(var.network-definition)  
-  name      = "vpc-${var.project-concept}-${lookup(var.var.network-definition,count.index)[0]}"
+  count     = length(var.network-definition)  
+  name      = "vpc-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}"
   auto_create_subnetworks = "false"
 }
 
 # Subnet
 resource "google_compute_subnetwork" "nets-intellibot-A" {
-  count     = lenth(var.network-definition)
+  count     = length(var.network-definition)
   region    = var.gcp-region
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.var.network-definition,count.index)[1]
-  name      = "net-${var.project-concept}-${lookup(var.var.network-definition,count.index)[0]}-A"  
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-A"  
 }
 resource "google_compute_subnetwork" "nets-intellibot-B" {
-  count     = lenth(var.network-definition)
+  count     = length(var.network-definition)
   region    = var.gcp-region
   network   = google_compute_network.vpc-intellibot[count.index].name
   ip_cidr_range = lookup(var.var.network-definition,count.index)[2]
-  name      = "net-${var.project-concept}-${lookup(var.var.network-definition,count.index)[0]}-B"  
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-B"  
 }
 /*
 module "vpc-intellibot-A" {
