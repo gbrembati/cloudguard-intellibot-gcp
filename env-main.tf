6631e1ece571200001e84a6239421c3dacdf030a
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-09-27 11:50:58 +0200

    adding preprod

--- env-main.tf
+++ env-main.tf
@@ -30,18 +30,35 @@ resource "google_compute_subnetwork" "nets-intellibot-c" {
   name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-c"  
 }
 
-resource "google_compute_network_peering" "peering-dev-to-preprod" {
+resource "google_compute_network_peering" "peering--to-preprod" {
   name         = "peering-${google_compute_network.vpc-intellibot[0].name}-to-${google_compute_network.vpc-intellibot[1].name}"
   network      = google_compute_network.vpc-intellibot[0].id
   peer_network = google_compute_network.vpc-intellibot[1].id
 }
+resource "google_compute_network_peering" "peering-preprod-to-dev" {
+  name         = "peering-${google_compute_network.vpc-intellibot[1].name}-to-${google_compute_network.vpc-intellibot[0].name}"
+  network      = google_compute_network.vpc-intellibot[1].id
+  peer_network = google_compute_network.vpc-intellibot[0].id
+}
+
 resource "google_compute_network_peering" "peering-dev-to-prod" {
   name         = "peering-${google_compute_network.vpc-intellibot[0].name}-to-${google_compute_network.vpc-intellibot[2].name}"
   network      = google_compute_network.vpc-intellibot[0].id
   peer_network = google_compute_network.vpc-intellibot[2].id
 }
+resource "google_compute_network_peering" "peering-prod-to-dev" {
+  name         = "peering-${google_compute_network.vpc-intellibot[2].name}-to-${google_compute_network.vpc-intellibot[0].name}"
+  network      = google_compute_network.vpc-intellibot[2].id
+  peer_network = google_compute_network.vpc-intellibot[0].id
+}
+
 resource "google_compute_network_peering" "peering-preprod-to-prod" {
   name         = "peering-${google_compute_network.vpc-intellibot[1].name}-to-${google_compute_network.vpc-intellibot[2].name}"
   network      = google_compute_network.vpc-intellibot[1].id
   peer_network = google_compute_network.vpc-intellibot[2].id
+}
+resource "google_compute_network_peering" "peering-prod-to-preprod" {
+  name         = "peering-${google_compute_network.vpc-intellibot[2].name}-to-${google_compute_network.vpc-intellibot[1].name}"
+  network      = google_compute_network.vpc-intellibot[2].id
+  peer_network = google_compute_network.vpc-intellibot[1].id
 }
