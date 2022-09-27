c5fea61f387ead6bf157d66d85f422ce37fdefa8
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-09-27 11:05:21 +0200

    first networks

--- env-main.tf
+++ env-main.tf
@@ -1,5 +1,4 @@
 
-
 resource "google_compute_network" "vpc-intellibot" {
   count     = length(var.network-definition)  
   name      = "vpc-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}"
@@ -11,77 +10,20 @@ resource "google_compute_subnetwork" "nets-intellibot-A" {
   count     = length(var.network-definition)
   region    = var.gcp-region
   network   = google_compute_network.vpc-intellibot[count.index].name
-  ip_cidr_range = lookup(var.var.network-definition,count.index)[1]
+  ip_cidr_range = lookup(var.network-definition,count.index)[1]
   name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-A"  
 }
 resource "google_compute_subnetwork" "nets-intellibot-B" {
   count     = length(var.network-definition)
   region    = var.gcp-region
   network   = google_compute_network.vpc-intellibot[count.index].name
-  ip_cidr_range = lookup(var.var.network-definition,count.index)[2]
+  ip_cidr_range = lookup(var.network-definition,count.index)[2]
   name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-B"  
 }
-/*
-module "vpc-intellibot-A" {
-  source  = "terraform-google-modules/network/google//modules/vpc"
-  version = "~> 2.0.0"
-
-  project_id   = var.gcp-project
-  network_name = "vpc-${var.project-concept}-A"
+resource "google_compute_subnetwork" "nets-intellibot-C" {
+  count     = length(var.network-definition)
+  region    = var.gcp-region
+  network   = google_compute_network.vpc-intellibot[count.index].name
+  ip_cidr_range = lookup(var.network-definition,count.index)[3]
+  name      = "net-${var.project-concept}-${lookup(var.network-definition,count.index)[0]}-B"  
 }
-module "nets-intellibot-A" {
-  source  = "terraform-google-modules/network/google//modules/subnets"
-  version = "~> 2.0.0"
-
-  project_id   = var.gcp-project
-  network_name = "vpc-${var.project-concept}-A"
-
-  subnets = [
-    {
-      subnet_name           = "net-${var.project-concept}-A-01"
-      subnet_ip             = "10.10.10.0/24"
-      subnet_region         = var.gcp-region
-    },
-    {
-      subnet_name           = "net-${var.project-concept}-A-02"
-      subnet_ip             = "10.10.20.0/24"
-      subnet_region         = var.gcp-region
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-      subnet_flow_logs_interval    = "INTERVAL_10_MIN"
-    }
-  ]
-}
-
-module "vpc-intellibot-B" {
-  source  = "terraform-google-modules/network/google//modules/vpc"
-  version = "~> 2.0.0"
-
-  project_id   = var.gcp-project
-  network_name = "vpc-${var.project-concept}-B"
-
-}
-module "nets-intellibot-B" {
-  source  = "terraform-google-modules/network/google//modules/subnets"
-  version = "~> 2.0.0"
-
-  project_id   = var.gcp-project
-  network_name = "vpc-${var.project-concept}-B"
-
-  subnets = [
-    {
-      subnet_name           = "net-${var.project-concept}-B-01"
-      subnet_ip             = "10.20.10.0/24"
-      subnet_region         = var.gcp-region
-    },
-    {
-      subnet_name           = "net-${var.project-concept}-B-02"
-      subnet_ip             = "10.20.20.0/24"
-      subnet_region         = var.gcp-region
-      subnet_private_access = "true"
-      subnet_flow_logs      = "true"
-      subnet_flow_logs_interval    = "INTERVAL_10_MIN"
-    }
-  ]
-}
-*/
